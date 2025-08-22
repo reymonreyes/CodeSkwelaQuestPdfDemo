14cfad4edec4ec9ec598426b8324d4fbc785804a
Reymon Reyes <elmoe01@gmail.com>
2025-08-22 17:36:51 +0800

    Add project files.

---
+++ QuestPdfConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Infrastructure;
+
+namespace QuestPdfConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestPDF.Settings.License = LicenseType.Community;
+
+            Document.Create(document =>
+            {
+                document.Page(page =>
+                {
+                    page.Content().Text("Hello PDF!");
+                });
+            }).GeneratePdfAndShow();
+        }
+    }
+}
